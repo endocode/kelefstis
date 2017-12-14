bfbac416f887cae24af27039cfb37addcbc3677e
Thomas Fricke <thomas@endocode.com>
2017-12-14 17:59:27 +0100

    made the code testable

--- kelefstis.go
+++ kelefstis.go
@@ -1,4 +1,4 @@
-package main
+package kelefstis
 
 import (
 	"fmt"
@@ -10,17 +10,14 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"path/filepath"
-	"github.com/endocode/kelefstis/check"
-	"text/template"
 	"io/ioutil"
 	"bytes"
 	"github.com/ghodss/yaml"
 	"time"
+
 )
 
-// simple k8s client that lists all available pods
-// it gets config from $HOME/.kube/config
-func main()  {
+func ClientSet(argv []string) (*kubernetes.Clientset,string, error){
 	usage:=`kelefstis.
 
 Usage:
@@ -32,57 +29,42 @@ Options:
 		check                 Template with the checks to run
 	--kubeconfig <config>
 `
-	arguments,_ := docopt.Parse(usage,nil,false,"kelefstis 0.1", false)
+	if argv == nil && len(os.Args) > 1 {
+		argv = os.Args[1:]
+	}
+	arguments,_ := docopt.Parse(usage,argv,false,"kelefstis 0.1", false)
 
 	if arguments["--help"].(bool)  || arguments["<check>"] == nil {
 		fmt.Printf(usage)
-		return
+		os.Exit(1)
 	}
 
+	fmt.Printf("-------------------------> %s", argv)
 
 	checkfile:=arguments["<check>"].(string)
+	checktemplate, err := ioutil.ReadFile(checkfile)
 
 	kubeconfig, ok := arguments["--kubeconfig"].(string)
 	if ! ok {
 		kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
 	}
 
-	checktemplate, err := ioutil.ReadFile(checkfile)
-	if err != nil {
-		fmt.Printf(usage)
-		return
-	}
-
 	fmt.Println("Using kubeconfig: ", kubeconfig)
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("   unusable, exiting")
+		os.Exit(3)
 	}
 
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		os.Exit(4)
 	}
-
-	chk := check.Check{true, clientset.CoreV1()}
-	tmpl, err := template.New("test").Parse(string(checktemplate))
-	if err != nil {
-		panic(err)
-	}
-	err = tmpl.Execute(os.Stdout, &chk)
-	if err != nil {
-		panic(err.Error())
-	}
-	/*
-	listNodes(clientset)
-	listPods(clientset)
-	listResource(clientset)
-	*/
-	listCRD(clientset, "stable.example.com", "v1", "rulecheckers","rules")
-
+	return clientset, string( checktemplate), err
 }
-func listPods(clientset *kubernetes.Clientset) (*apiv1.PodList, error) {
+
+func ListPods(clientset *kubernetes.Clientset) (*apiv1.PodList, error) {
 	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
@@ -97,7 +79,7 @@ func listPods(clientset *kubernetes.Clientset) (*apiv1.PodList, error) {
 	return pods, err
 }
 
-func listNodes(clientset *kubernetes.Clientset) (*apiv1.NodeList, error) {
+func ListNodes(clientset *kubernetes.Clientset) (*apiv1.NodeList, error) {
 	nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err!=nil {
 		panic(err.Error())
@@ -112,7 +94,7 @@ func listNodes(clientset *kubernetes.Clientset) (*apiv1.NodeList, error) {
 	return nodes, err
 }
 
-func listResource(clientset *kubernetes.Clientset)  {
+func ListResource(clientset *kubernetes.Clientset)  {
 	raw ,err := clientset.CoreV1().
 		RESTClient().Get().
 		Resource("").DoRaw()
@@ -161,7 +143,7 @@ type RuleChecker struct {
 	} `json:"spec"`
 }
 
-func listCRD(clientset *kubernetes.Clientset,group string, version string, crd string, resource string) {
+func ListCRD(clientset *kubernetes.Clientset,group string, version string, crd string, resource string) {
 	rules := clientset.CoreV1().
 		RESTClient().
 			Get().
---
+++ test/kelefstis_test.go
@@ -0,0 +1,35 @@
+package test
+
+
+import (
+"testing"
+"github.com/stretchr/testify/assert"
+
+	"github.com/endocode/kelefstis"
+)
+
+func TestSomething(t *testing.T) {
+	clientset, checktemplate, err := kelefstis.ClientSet([]string{"/home/thomas/import/go/src/github.com/endocode/kelefstis/check.templ"})
+	assert.NotNil(t, clientset)
+	assert.NotNil(t,checktemplate)
+	assert.Nil(t,err)
+
+	// assert equality
+	assert.Equal(t, 123, 123, "they should be equal")
+
+	// assert inequality
+	assert.NotEqual(t, 123, 456, "they should not be equal")
+
+	// assert for nil (good for errors)
+	assert.Nil(t, nil)
+
+	// assert for not nil (good when you expect something)
+	if assert.NotNil(t, 2) {
+
+		// now we know that object isn't nil, we are safe to make
+		// further assertions without causing any errors
+		assert.Equal(t, "Something", "Something")
+
+	}
+
+}

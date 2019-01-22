0838cc46cc8e3a2df1ec44df117939b2fe641e35
Thomas Fricke <thomas@endocode.com>
2019-01-22 15:58:41 +0100

    seperated results

---
+++ result.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/endocode/goju"
+	"github.com/golang/glog"
+)
+
+type ReportTreeCheck struct {
+	goju.TreeCheck
+}
+
+// Report shows the results of a TreeCheck on glog with
+// the appropriate level
+func (r *ReportTreeCheck) Report(level glog.Level) {
+	glog.V(level).Infof("Errors       : %d\n", r.ErrorHistory.Len())
+	glog.V(level).Infof("Checks   true: %d\n", r.TrueCounter)
+	glog.V(level).Infof("Checks  false: %d\n", r.FalseCounter)
+}
+
+// Evaluate reports true, if no errors and no false results have been found,
+// and at least one true result
+func (r *ReportTreeCheck) Evaluate() bool {
+	return r.ErrorHistory.Len() == 0 && r.FalseCounter == 0 && r.TrueCounter > 0
+}
